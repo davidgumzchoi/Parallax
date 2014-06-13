1b25b8531116bedc1f9dff1e13e862e155fd0d65
GumZ <davidgumzchoi@gmail.com>
2014-06-13 09:33:36 -0700

    fixed dynamic video player

--- js/application.js
+++ js/application.js
@@ -27,24 +27,3 @@ $(function() {
     }
   });
 });
-
-// Fluid Width Video
-var $allVideos = $("iframe[src^='http://www.youtube.com']");
-var $fluidEl = $("body");
-
-$allVideos.each(function() {
-  $(this)
-    .data('aspectRatio', this.height / this.width)
-    .removeAttr('height')
-    .removeAttr('width');
-});
-
-$(window).resize(function() {
-  var newWidth = $fluidEl.width();
-  $allVideos.each(function() {
-    var $el = $(this);
-    $el
-      .width(newWidth)
-      .height(newWidth * $el.data('aspectRatio'));
-  });
-}).resize();
